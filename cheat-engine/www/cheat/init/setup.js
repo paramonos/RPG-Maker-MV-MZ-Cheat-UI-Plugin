b33c2209d12134e8d26777a276545c5527d42012
paramonos <99193603+paramonos@users.noreply.github.com>
2022-02-26 17:18:35 +0900

    feature/1.0.0: Supports RPG Maker MZ
    - Separate js files to support MV and MZ at the same time.

--- cheat-engine/www/cheat/init/setup.js
+++ cheat-engine/www/cheat/init/setup.js
@@ -3,10 +3,10 @@
 // import 'https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js'
 // import 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js'
 
-import './libs/vue.js'
-import './libs/vuetify.js'
+import '../libs/vue.js'
+import '../libs/vuetify.js'
 
-import MainComponent from './MainComponent.js'
+import MainComponent from '../MainComponent.js'
 
 // initialize vue
 new Vue({
